c5ed91b909b9d3701c49eb7e3a18bdb5c3a7c6ad
ZakirovaVera <zakirovavk@gmail.com>
2022-12-01 12:09:23 +0500

    решение задачи удалить четные числа

--- task2.java
+++ task2.java
@@ -1,6 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+
 // Пусть дан произвольный список целых чисел, удалить из него четные числа
 public class task2 {
     public static void main(String[] args) {
+        List<Integer> wholeТumbers = new ArrayList<>();
+        int size = 10;
+
+        FillRandomList(wholeТumbers, size);
+        System.out.println();
+        RemoveEvenNumbers(wholeТumbers);
         
+    }
+    public static void FillRandomList(List<Integer> arr, int size) {
+        for (int i = 0; i < size; i++) {
+            arr.add((int)(Math.random() * (20 + 1 - 0) + 0));
+        }
+        for (var el : arr) {
+            System.out.print(el+" ");
+        }
+    }
+    public static void RemoveEvenNumbers(List<Integer> arr) {
+        List<Integer> arrNotEvenNum = new ArrayList<>();
+        for(int i = 0; i < arr.size(); i++) {
+            if(arr.get(i) % 2 == 1){
+                arrNotEvenNum.add(arr.get(i));
+            }
+        }
+        for (var el : arrNotEvenNum) {
+             System.out.print(el+" ");
+        }  
     }
 }
